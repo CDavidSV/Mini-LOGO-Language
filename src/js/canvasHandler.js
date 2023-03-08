0cc90f527f1db16b627b0854f36f6443fdeb2133
CDavidSV <88672259+CDavidSV@users.noreply.github.com>
2023-03-08 00:09:32 -0600

    Changed some colors and styles
    - Implemented the animate function for the canvas.

--- src/js/canvasHandler.js
+++ src/js/canvasHandler.js
@@ -4,19 +4,78 @@ const ctx = canvas.getContext('2d');
 const canvasContainer = document.querySelector('.draw-area');
 
 // Variables
-const mainColor = '#0F0F0F';
+const interval = 1000 / 60;
+let then = Date.now();
+let now;
+let delta;
+const mainCanvasColor = '#0F0F0F';
+const penColors = ["red", "blue", "green", "yellow", "orange", "purple", "black", "white", "gray", "brown", "pink", "turquoise", "lavender", "teal", "maroon", "navy", "olive", "coral", "beige", "gold"];
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-ctx.fillStyle = mainColor;
+ctx.fillStyle = mainCanvasColor;
 
 // Events
 window.addEventListener('resize', resizeCanvas);
 
+class Robot {
+    constructor() {
+
+    }
+
+    update() {
+
+    }
+
+    move(direction) {
+
+    }
+
+    rotate(angle) {
+
+    }
+
+    pickupPen() {
+
+    }
+
+    dropPen(){
+
+    }
+    
+    changeColor(color) {
+
+    }
+
+    center() {
+
+    }
+}
+
 // Functions
+
+// Clears the canvas.
+function clearScreen() {
+    context.globalCompositeOperation = 'source-over';
+    context.fillStyle = mainCanvasColor;
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    context.globalCompositeOperation = "lighter";
+}
+
+// Main animate function.
+function animate() {
+    // Update the rockets position over time.
+    requestAnimationFrame(animate);
+
+    now = Date.now();
+    delta = now - then
+    if (!(delta > interval)) return;
+}
 
 // Resizes the canvas when user resizes their browser window.
 function resizeCanvas() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 }
+
+animate();
